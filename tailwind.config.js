d1efc79668488e4031bc296bf15c0e53fdfccd23
NullBy1e <68380685+NullBy1e@users.noreply.github.com>
2022-08-14 21:06:24 +0200

    V1

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '300px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
