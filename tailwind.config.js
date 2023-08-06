fca4495207431b05894fd117c9ba2982ff4149ac
NullBy1e <bartosz.sasal@quasiphi.com>
2023-08-06 22:49:34 +0200

    Nowy design strony

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./*.{html,js}"],
+	theme: {
+		extend: {},
+		colors: {
+			white: "#ffffff",
+			blue: {
+				50: "#fcfdfd",
+				100: "#c9fafe",
+				200: "#94effb",
+				300: "#57d9f1",
+				400: "#24bbdd",
+				500: "#0b9dc1",
+				600: "#067a9b",
+				700: "#09617c",
+				800: "#0d4d62",
+				900: "#104151",
+				950: "#00111a",
+			},
+		},
+	},
+	plugins: [],
+};
